eebd4824b0119c29b75bba4b2e93941223cc6c18
Niels Bunkenburg <stu114713@informatik.uni-kiel.de>
2017-03-22 21:31:46 +0100

    Add proof (attempt)s

--- src/Env.v
+++ src/Env.v
@@ -40,15 +40,9 @@ Section Properties.
       lookup eqA Sigma k = v2 ->
       v1 = v2.
   Proof.
-  Admitted.
-
-  Lemma lookupT_deterministic :
-    forall (A B : Type) (Sigma : EnvironmentT A B) (k : A) (v1 v2 : B),
-      Sigma k = v1 ->
-      Sigma k = v2 ->
-      v1 = v2.
-  Proof.
-  Admitted.
+    intros A B eqA Sigma k v1 v2 H H0.
+    rewrite <- H. rewrite <- H0. reflexivity.
+  Qed.
 
   Lemma lookupP_deterministic :
     forall (A B : Type) (Sigma : EnvironmentP A B) (k : A) (v1 v2 : option B),
@@ -56,14 +50,20 @@ Section Properties.
       Sigma k = v2 ->
       v1 = v2.
   Proof.
-  Admitted.
+    intros A B Sigma k v1 v2 H H0. rewrite <- H. auto.
+  Qed.
 
   Lemma update_lookup1 :
     forall (A B : Type) (eqA : A -> A -> bool) (Sigma : EnvironmentL A B) (k : A) (v : B),
       (forall x : A, eqA x x = true) ->
       lookup eqA (update eqA Sigma k v) k = Some v.
   Proof.
-  Admitted.
+    intros A B eqA Sigma k v H. induction Sigma.
+    - simpl. rewrite H. reflexivity.
+    - destruct a. simpl. destruct (eqA a k) eqn:H0.
+      * simpl. rewrite H0. reflexivity.
+      * simpl. rewrite H0. apply IHSigma.
+  Qed.
 
   Lemma update_lookup2 :
     forall (A B : Type) (eqA : A -> A -> bool) (Sigma : EnvironmentL A B) (k1 k2 : A) (v1 v2 : B),
@@ -71,13 +71,22 @@ Section Properties.
       eqA k2 k1 = false ->
       lookup eqA (update eqA Sigma k2 v2) k1 = lookup eqA Sigma k1.
   Proof.
-  Admitted.
+    intros A B eqA Sigma k1 k2 v1 v2 H H0. induction Sigma.
+    - simpl. rewrite H0. reflexivity.
+    - destruct a. simpl. destruct (eqA a k2) eqn:H1.
+      * assert (eqA a k1 = false). Focus 2.
+      * rewrite H2. simpl. rewrite H2. reflexivity. Admitted.
 
   Lemma union_lookup :
     forall (A B : Type) (eqA : A -> A -> bool) (Sigma Delta : EnvironmentL A B) (k : A),
       lookup eqA Delta k = None ->
       lookup eqA (union Delta Sigma ) k = lookup eqA Sigma k.
   Proof.
+    intros A B eqA Sigma Delta k H. induction Delta.
+    - unfold union. rewrite app_nil_l. reflexivity.
+    - unfold union. rewrite <- app_comm_cons. destruct a.
+      * simpl. assert (eqA a k = false). Focus 2.
+      * rewrite H0. apply IHDelta. rewrite <- H. simpl. rewrite H0. reflexivity.
   Admitted.
 
 End Properties.
