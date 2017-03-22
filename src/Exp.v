40a0a41737440f04da8a8b8251ac3d1c0a3c2307
Sandra Dylus <sad@informatik.uni-kiel.de>
2017-03-22 15:29:57 +0100

    Improve expression data type as needed in example and add notations

--- src/Exp.v
+++ src/Exp.v
@@ -1,21 +1,16 @@
-Section Exp.
+Inductive AExp : Type :=
+| ANum  : nat  -> AExp
+| AVar  : nat -> AExp
+| APlus  : AExp -> AExp -> AExp
+| AMinus : AExp -> AExp -> AExp
+| AMult  : AExp -> AExp -> AExp.
 
-  Variable ID : Type.
+Inductive BExp : Type :=
+| BTrue  : BExp
+| BFalse : BExp
+| BLess  : AExp -> AExp -> BExp.
 
-  Inductive AExp : Type :=
-    | ANum  : nat  -> AExp
-    | AVar  : ID -> AExp
-    | APlus  : AExp -> AExp -> AExp
-    | AMinus : AExp -> AExp -> AExp
-    | AMult  : AExp -> AExp -> AExp.
-
-  Inductive BExp : Type :=
-    | BTrue  : BExp
-    | BFalse : BExp
-    | BVar   : ID -> BExp
-    | BEq  : AExp -> AExp -> BExp
-    | BLe  : AExp -> AExp -> BExp
-    | BNot : BExp -> BExp
-    | BAnd : BExp -> BExp -> BExp.
-
-End Exp.
+Notation "e1 :<: e2" := (BLess e1 e2) (at level 40, left associativity).
+Notation "e1 :+: e2" := (APlus e1 e2) (at level 40, left associativity).
+Notation "e1 :-: e2" := (AMinus e1 e2) (at level 40, left associativity).
+Notation "e1 :*: e2" := (AMult e1 e2) (at level 40, left associativity).
