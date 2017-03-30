0f40c70aee456b969d262b2847984a295d1fd468
Niels Bunkenburg <stu114713@informatik.uni-kiel.de>
2017-03-30 15:34:58 +0200

    Adapt SOS exercise S2/S4

--- src/Exercises/S2/A4.v
+++ src/Exercises/S2/A4.v
@@ -1,29 +1,34 @@
-Require Import SOS Lists.List.
+Require Import PPS.Exercises.S2.ABExp_Let.
+Require Import PPS.SOS.SOS.
+Require Import Lists.List.
+Set Implicit Arguments.
+
+Import SOS_Notation.
 Import ListNotations.
 
 Definition x := 0.
 Definition y := 1.
 Definition z := 2.
 
-Definition Sigma := [(x,(VInt 5)); (y,(VInt 3))].
-Definition exp := Op (Var y) mult (Let z (Op (Num (VInt 4)) plus (Var x))
-                                         (Op (Var z) sub (Num (VInt 7)))).
+Definition Sigma := [(x, 5); (y, 3)].
+Definition exp := (AVar y) :*: (Let z :=: (ANum 4) :+: (AVar x)
+                                In (AVar z) :-: (ANum 7)).
 
-Example e : Sigma |- exp ::: VInt 6.
+Example e : Sigma |-a exp ⇓ 6.
 Proof. apply EvMult with (v1 := 3) (v2 := 2).
        - apply EvVar. reflexivity.
        - eapply EvLet.
-         * apply EvPlus with (v1 := 4) (v2 := 5).
-           + apply EvNum.
-           + apply EvVar. reflexivity.
-           + reflexivity.
-         * reflexivity.
-         * apply EvSub with (v1 := 9) (v2 := 7).
-           + apply EvVar. reflexivity.
-           + apply EvNum.
-           + reflexivity.
+         + admit.
+         + apply EvPlus with (v1 := 4) (v2 := 5).
+           * apply EvNum.
+           * apply EvVar. reflexivity.
+           * reflexivity.
+         + apply EvSub with (v1 := 9) (v2 := 7).
+           * apply EvVar. reflexivity.
+           * apply EvNum.
+           * reflexivity.
        - reflexivity.
 Qed.
 
-Example e2 : Sigma |- exp ::: VInt 6.
+Example e2 : Sigma |-a exp ⇓ 6.
 Proof. repeat econstructor. Qed.
---
+++ src/Exercises/S2/ABExp_Let.v
@@ -0,0 +1,106 @@
+Require Import PPS.SOS.SOS.
+Require Export PPS.Exercises.S2.Exp_Let.
+Require Import PPS.Env.
+
+Set Implicit Arguments.
+
+Reserved Notation "Sigma '|-a' e ⇓ v" (at level 40).
+Reserved Notation "Sigma '|-b' e ⇓ v" (at level 40).
+
+Section ArithRelation.
+
+  Inductive aevalR : AExp -> SOS_Context nat -> nat -> Prop :=
+  | EvNum  : forall Sigma n, Sigma |-a ANum n ⇓ n
+  | EvVar  : forall Sigma x v,
+      SOS_lookup Sigma x = Some v ->
+      Sigma |-a AVar x ⇓ v
+  | EvPlus : forall Sigma e1 e2 v1 v2 v,
+      Sigma |-a e1 ⇓ v1 ->
+      Sigma |-a e2 ⇓ v2 ->
+      v = v1 + v2 ->
+      Sigma |-a APlus e1 e2 ⇓ v
+  | EvSub : forall Sigma e1 e2 v1 v2 v,
+      Sigma |-a e1 ⇓ v1 ->
+      Sigma |-a e2 ⇓ v2 ->
+      v = v1 - v2 ->
+      Sigma |-a AMinus e1 e2 ⇓ v
+  | EvMult : forall Sigma e1 e2 v1 v2 v,
+      Sigma |-a e1 ⇓ v1 ->
+      Sigma |-a e2 ⇓ v2 ->
+      v = v1 * v2 ->
+      Sigma |-a AMult e1 e2 ⇓ v
+  | EvLet : forall Sigma (Delta : SOS_Context nat) x e1 e2 v1 v2,
+      Sigma |-a e1 ⇓ v1 ->
+      let Delta := add Sigma x v1
+      in Delta |-a e2 ⇓ v2 ->
+      Sigma |-a ALet x e1 e2 ⇓ v2
+  where "Sigma '|-a' e ⇓ v" := (aevalR e Sigma v).
+
+End ArithRelation.
+
+Section ArithFunction.
+
+  Fixpoint aevalF (e : AExp) (c0 : SOS_Context nat) : option nat :=
+    match e with
+    | ANum n => Some n
+    | AVar x => SOS_lookup c0 x
+    | APlus e1 e2 =>
+      match aevalF e1 c0, aevalF e2 c0 with
+      | Some v1, Some v2 =>
+        Some (v1 + v2)
+      | _,_ => None
+      end
+    | AMult e1 e2 =>
+      match aevalF e1 c0, aevalF e2 c0 with
+      | Some v1, Some v2 =>
+        Some (v1 * v2)
+      | _,_ => None
+      end
+    | AMinus e1 e2 =>
+      match aevalF e1 c0, aevalF e2 c0 with
+      | Some v1, Some v2 =>
+        Some (v1 - v2)
+      | _,_ => None
+      end
+    | ALet x e1 e2 =>
+      match aevalF e1 c0 with
+      | Some v1 =>
+        aevalF e2 (add c0 x v1)
+      | _ => None
+      end
+    end.
+
+End ArithFunction.
+
+Notation "Sigma '|-a' e ⇓ v" := (aevalR e Sigma v).
+
+Section BoolRelation.
+
+  Inductive bevalR : BExp -> SOS_Context nat -> bool -> Prop :=
+  | EvT : forall Sigma, Sigma |-b BTrue ⇓ true
+  | EvF : forall Sigma, Sigma |-b BFalse ⇓ false
+  | EvLess : forall Sigma e1 e2 v1 v2 b,
+      Sigma |-a e1 ⇓ v1 ->
+      Sigma |-a e2 ⇓ v2 ->
+      b = Nat.leb v1 v2 ->
+      Sigma |-b BLess e1 e2 ⇓ b
+  where "Sigma '|-b' e ⇓ v" := (bevalR e Sigma v).
+
+End BoolRelation.
+
+Section BoolFunction.
+
+  Fixpoint bevalF (be : BExp) (c0 : SOS_Context nat) : option bool :=
+    match be with
+    | BTrue => Some true
+    | BFalse => Some false
+    | BLess e1 e2 =>
+      match aevalF e1 c0, aevalF e2 c0 with
+      | Some v1, Some v2 => Some (Nat.leb v1 v2)
+      | _,_ => None
+      end
+    end.
+
+End BoolFunction.
+
+Notation "Sigma '|-b' e ⇓ v" := (bevalR e Sigma v).
